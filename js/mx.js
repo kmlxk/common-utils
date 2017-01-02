51951eadffcb16e3c0d64cf2faeccb07d9fb7d8f
kmlxk <kmlxk@163.com>
2017-01-02 10:44:51 +0800

    mx.js增加注释 Mx.ajax.get/post

--- js/mx.js
+++ js/mx.js
@@ -1,4 +1,9 @@
 Mx = {
+    /**
+     * 定义命名空间
+     * @param {type} names 可以用.分割的命名空间字符串
+     * @returns {undefined}
+     */
     namespace: function(names) {
         var sep = names.split('.'),
                 i, scope = window;
@@ -9,6 +14,12 @@ Mx = {
             scope = scope[sep[i]];
         }
     },
+    /**
+     * 向src中未定义的字段中写入默认初始化数组对应值
+     * @param {type} src
+     * @param {type} defaults
+     * @returns {unresolved}
+     */
     applyDefault: function(src, defaults) {
         for (var k in defaults) {
             if (typeof src[k] === 'undefined' && typeof defaults[k] !== 'undefined') {
@@ -17,6 +28,12 @@ Mx = {
         }
         return src;
     },
+    /**
+     * 将下划线分隔的变量转换为驼峰命名法
+     * @param {type} string
+     * @param {type} upperFirst
+     * @returns {unresolved}
+     */
     camelCase: function(string, upperFirst) {
         var rdashAlpha = /_([\da-z])/gi;
         if (typeof upperFirst === 'undefined') {
@@ -29,6 +46,13 @@ Mx = {
             return letter.toUpperCase();
         });
     },
+    /**
+     * 动态加载js
+     * @param {type} id
+     * @param {type} fileUrl
+     * @param {type} fnComplete
+     * @returns {undefined}
+     */
     loadJS: function(id, fileUrl, fnComplete) {
         console.log('loadJS', id, fileUrl);
         var scriptTag = document.getElementById(id);
@@ -51,6 +75,11 @@ Mx = {
         }
         oHead.appendChild(oScript);
     },
+    /**
+     * 动态创建div
+     * @param {type} id
+     * @returns {undefined}
+     */
     createDivById: function(id) {
         var el = document.getElementById(id);
         if (el) {
@@ -60,6 +89,11 @@ Mx = {
         el.id = id;
         document.body.appendChild(el);
     },
+    /**
+     * 转换URL中的参数
+     * @param {type} url
+     * @returns {object}
+     */
     parseUrlParameters: function(url) { //定义函数
         if (typeof url === 'undefined') {
             return {};
@@ -71,6 +105,11 @@ Mx = {
         });
         return params;//返回这个数组.
     },
+    /**
+     * 计算hash
+     * @param {type} str
+     * @returns {Number}
+     */
     hashCode: function(str) {
         var hash = 0;
         if (str == null || str.length == 0)
@@ -83,6 +122,11 @@ Mx = {
         return hash;
     }
 };
+/**
+ * 日期格式化 示例：(new Date()).format('YYYY-MM-dd hh:mm:ss')
+ * @param {type} format 'YYYY-MM-dd hh:mm:ss'
+ * @returns {unresolved}
+ */
 Date.prototype.format = function(format) {
     var o = {
         "M+": this.getMonth() + 1,
@@ -110,22 +154,42 @@ Date.prototype.format = function(format) {
     }
     return format;
 };
+/**
+ * 获取Unix时间戳(1970秒)
+ * @returns {Number}
+ */
 Mx.getTimestamp = function() {
     var timestamp = Math.round(new Date().getTime() / 1000);
     return timestamp;
 };
+/**
+ * 根据Unix时间戳获取日期时间字符串
+ * @param {type} timestamp
+ * @returns {String}
+ */
 Mx.getDateTimeStr = function(timestamp) {
     if (timestamp == null || timestamp == 0) {
         return '';
     }
     return (new Date(timestamp * 1000)).format("yyyy-MM-dd hh:mm:ss");
 };
+/**
+ * 根据Unix时间戳获取日期字符串
+ * @param {type} timestamp
+ * @returns {String}
+ */
 Mx.getDateStr = function(timestamp) {
     if (timestamp == null || timestamp == 0) {
         return '';
     }
     return (new Date(timestamp * 1000)).format("yyyy-MM-dd");
 };
+/**
+ * 输出调试信息
+ * @param {type} msg
+ * @param {type} category
+ * @returns {undefined}
+ */
 Mx.debug = function(msg, category) {
     if (Mx.debug.mode == 0) {
         return;
@@ -151,9 +215,17 @@ Mx.debug = function(msg, category) {
     }
     console.log('[' + category + ']' + msg);
 }
+// 全局变量 ，1以firebug console方式、 2以alert弹出信息方式
 Mx.debug.mode = 1;
+// 全局变量 ，跳过不显示的调试信息类型
 Mx.debug.filter = ['timer', 'dump'];
+
 Mx.namespace('Mx.model');
+/**
+ * 解析简易kv数据  "key,value|key,value|key,value"
+ * @param {type} str
+ * @returns {Array|Mx.model.fromString.ret}
+ */
 Mx.model.fromString = function(str) {
     var ret = [],
             lines = str.split(/\|/gi),
@@ -171,8 +243,16 @@ Mx.model.fromString = function(str) {
     }
     return ret;
 };
+
 Mx.namespace('Mx.array');
 Mx.array = {
+    /**
+     * 搜索数组，支持一维、二维数组
+     * @param {type} ar 待查数组
+     * @param {type} key 二维数组中的键
+     * @param {type} value 待查的值
+     * @returns {Number} 返回索引下标
+     */
     search: function(ar, key, value) {
         // 在一维数组中查找
         if (typeof value == 'undefined') {
@@ -187,6 +267,12 @@ Mx.array = {
         }
         return -1;
     },
+    /**
+     * 删除指定位置的元素
+     * @param {type} ar
+     * @param {type} index
+     * @returns {unresolved}
+     */
     removeAt: function(ar, index) {
         if (index < 0) {
             return ar;
@@ -216,6 +302,12 @@ Mx.array = {
         }
         return ret;
     },
+    /**
+     * 过滤数组
+     * @param {type} ar
+     * @param {type} fnCallback
+     * @returns {Array|Mx.array.filter.ret}
+     */
     filter: function(ar, fnCallback) {
         var i = 0,
                 len = ar.length,
@@ -227,6 +319,14 @@ Mx.array = {
         }
         return ret;
     },
+    /**
+     * 将平面数组转换为树形结构
+     * @param {type} list
+     * @param {type} idField
+     * @param {type} parentField
+     * @param {type} depthField
+     * @returns {Mx.array.toTree.Anonym$5|Number|Window}
+     */
     toTree: function(list, idField, parentField, depthField) {
         if (typeof depthField === 'undefined') {
             depthField = 'depth';
@@ -267,17 +367,56 @@ Mx.array = {
             return {children: roots, name: ''};
         }
         return root;
+    },
+    /**
+     * 将数组转换为以某一个字段为主键的快查hash表
+     * @param {type} arr
+     * @param {type} key
+     * @returns {Mx.array.toDictionary.arr}
+     */
+    toDictionary: function(arr, key) {
+        var i = 0,
+                len = arr.length,
+                ret = {};
+        for (; i < len; i++) {
+            ret[arr[i][key]] = arr[i];
+        }
+        return ret;
+    },
+    /**
+     * 循环数组
+     * @param {type} arr
+     * @param {type} fn
+     * @returns {undefined}
+     */
+    each: function(arr, fn) {
+        var i = 0,
+                len = arr.length,
+                ret = {};
+        for (; i < len; i++) {
+            fn.apply(this, [arr[i]]);
+        }
     }
 };
 
 Mx.namespace('Mx.web');
 Mx.web = {
+    /**
+     * 通过创建iframe下载文件
+     * @param {type} url
+     * @returns {undefined}
+     */
     downloadFile: function(url) {
         var elemIF = document.createElement("iframe");
         elemIF.src = url;
         elemIF.style.display = "none";
         document.body.appendChild(elemIF);
     },
+    /**
+     * 获取URL参数变量
+     * @param {type} name
+     * @returns {unresolved}
+     */
     getQueryString: function(name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); // 匹配目标参数
         var result = window.location.search.substr(1).match(reg);  // 对querystring匹配目标参数
@@ -287,6 +426,10 @@ Mx.web = {
             return null;
         }
     },
+    /**
+     * 获取所有URL参数变量
+     * @returns {object}
+     */
     getQueryParameters: function() {
         var qs = location.search.substr(1), // 获取url中"?"符后的字串   
                 args = {}, // 保存参数数据的对象
@@ -307,6 +450,12 @@ Mx.web = {
 };
 Mx.namespace('Mx.dictionary');
 Mx.dictionary = {
+    /**
+     * 在字典中查找数据，返回值主键名
+     * @param {type} kv
+     * @param {type} value
+     * @returns {string} 主键名
+     */
     find: function(kv, value) {
         for (var prop in kv) {
             if (kv.hasOwnProperty(prop)) {
@@ -318,8 +467,64 @@ Mx.dictionary = {
     }
 };
 
+Mx.namespace('Mx.ajax');
+Mx.ajax = {
+    /**
+     * 封装了错误回调函数的jQuery.ajax请求
+     * @param {type} requestType 请求类型, POST/GET
+     * @param {type} url  请求URL
+     * @param {type} params 请求参数
+     * @param {type} success 成功回调 function(json)
+     * @param {type} error 错误回调 function(XMLHttpRequest, textStatus, errorThrown)
+     * @param {type} resultType 返回值类型text/html/json
+     * @returns {undefined}
+     */
+    request: function(requestType, url, params, success, error, resultType) {
+        if (typeof resultType == 'undefined') {
+            resultType = 'json';
+        }
+        jQuery.ajax({
+            url: url,
+            type: requestType,
+            data: params,
+            dataType: resultType,
+            success: success,
+            error: error
+        });
+    },
+    /**
+     * 封装了错误回调函数的jQuery.ajax.get请求
+     * @param {type} url  请求URL
+     * @param {type} params 请求参数
+     * @param {type} success 成功回调 function(json)
+     * @param {type} error 错误回调 function(XMLHttpRequest, textStatus, errorThrown)
+     * @param {type} resultType 返回值类型text/html/json
+     * @returns {undefined}
+     */
+    get: function(url, params, success, error, resultType) {
+        Mx.ajax.request('get', url, params, success, error, resultType);
+    },
+    /**
+     * 封装了错误回调函数的jQuery.ajax.post请求
+     * @param {type} url  请求URL
+     * @param {type} params 请求参数
+     * @param {type} success 成功回调 function(json)
+     * @param {type} error 错误回调 function(XMLHttpRequest, textStatus, errorThrown)
+     * @param {type} resultType 返回值类型text/html/json
+     * @returns {undefined}
+     */
+    post: function(url, params, success, error, resultType) {
+        Mx.ajax.request('post', url, params, success, error, resultType);
+    }
+}
+
 Mx.namespace('Mx.form');
 Mx.form = {
+    /**
+     * 获取所有jQuery对象字段的数值
+     * @param {type} fields
+     * @returns {unresolved}
+     */
     getFieldsValue: function(fields) {
         var i = 0,
                 len = fields.length,
@@ -333,6 +538,13 @@ Mx.form = {
         }
         return ret;
     },
+    /**
+     * 按照name设置所有jQuery对象字段的数值
+     * @param {type} $parent
+     * @param {type} kv
+     * @param {type} tag
+     * @returns {undefined}
+     */
     setValues: function($parent, kv, tag) {
         var k,
                 $field;
@@ -343,6 +555,12 @@ Mx.form = {
             $parent.find(tag + '[name=' + k + ']').val(kv[k]);
         }
     },
+    /**
+     * 导出父元素下所有的待提交数组，通过data-key/name表示提交的key
+     * @param {type} $parent 父元素
+     * @param {type} submitSelector 子提交选择器，默认 '.submitvalue'
+     * @returns {value}
+     */
     exportValues: function($parent, submitSelector) {
         var values = {};
         if (typeof (submitSelector) == 'undefined') {
@@ -350,10 +568,25 @@ Mx.form = {
         }
         $parent.find(submitSelector).each(function() {
             var $input = $(this);
-            values[$input.data('key')] = $input.val();
+            var key = $input.data('key');
+            if (typeof (key) == 'undefined') {
+                key = $input.attr('name');
+            }
+            var value = $input.val();
+            if (typeof (value) == 'undefined') {
+                value = $input.text();
+            }
+            values[key] = value;
         });
         return values;
     },
+    /**
+     * 向父元素下所有的数组导入数据，通过data-key/name表示提交的key
+     * @param {type} $parent
+     * @param {type} values
+     * @param {type} submitSelector
+     * @returns {undefined}
+     */
     importValues: function($parent, values, submitSelector) {
         if (typeof (submitSelector) == 'undefined') {
             submitSelector = '.submitvalue';
@@ -364,6 +597,12 @@ Mx.form = {
             $input.val(values[key]);
         });
     },
+    /**
+     * 重置
+     * @param {type} $parent
+     * @param {type} submitSelector
+     * @returns {undefined}
+     */
     resetValues: function($parent, submitSelector) {
         if (typeof (submitSelector) == 'undefined') {
             submitSelector = '.submitvalue';
@@ -373,6 +612,12 @@ Mx.form = {
             $input.val('');
         });
     },
+    /**
+     * 通过object创建select的options代码
+     * @param {Dictionary} map 字典
+     * @param {string} defaultText 默认选项文字
+     * @param {Array} selected 已选择的选项，数组表示，可多选
+     * */
     buildSelectHtml: function(map, defaultText, selected) {
         if (typeof (selected) == 'undefined') {
             selected = [];
@@ -393,6 +638,15 @@ Mx.form = {
         }
         return html.join('');
     },
+    /**
+     * 通过树形结构创建select的options代码，可带缩进
+     * @param {type} tree
+     * @param {type} defaultText
+     * @param {type} valueKey
+     * @param {type} textKey
+     * @param {type} indentText
+     * @returns {String}
+     */
     buildSelectHtmlFromTree: function(tree, defaultText, valueKey, textKey, indentText) {
         indentText = indentText || '　';
         valueKey = valueKey || 'id';
@@ -416,6 +670,12 @@ Mx.form = {
         }
         return html.join('');
     },
+    /**
+     * 通过object创建checkboxs代码
+     * @param {type} map
+     * @param {type} attrHtml
+     * @returns {String}
+     */
     buildCheckboxHtml: function(map, attrHtml) {
         var html = [];
         var k = null;
@@ -426,6 +686,11 @@ Mx.form = {
         }
         return html.join('');
     },
+    /**
+     * 获取所有checkbox的数值
+     * @param {type} $items
+     * @returns {Array|Mx.form.getCheckboxValue.ret}
+     */
     getCheckboxValue: function($items) {
         var i = 0,
                 len = $items.length,
@@ -441,6 +706,11 @@ Mx.form = {
         }
         return ret;
     },
+    /**
+     * 获取所有radio的数值
+     * @param {type} $items
+     * @returns {Array|Mx.form.getCheckboxValue.ret}
+     */
     getRadioValue: function($items) {
         var i = 0,
                 len = $items.length,
@@ -457,11 +727,21 @@ Mx.form = {
     }
 };
 Mx.namespace('Mx.json');
+/**
+ * 解析json字符串，类似JSON.parse
+ * @param {type} str
+ * @returns {Function}
+ */
 Mx.json.toObject = function(str) {
     return (new Function("return " + str))();
 };
 
 Mx.namespace('Mx.browser');
+/**
+ * 支持在WebBrowser控件中的alert提示信息
+ * @param {type} message
+ * @returns {unresolved}
+ */
 Mx.browser.alert = function(message) {
     var domain = window;
     if (navigator.userAgent.indexOf('OpenWebKitSharp') >= 0) {
@@ -470,22 +750,23 @@ Mx.browser.alert = function(message) {
     return domain.alert(message);
 };
 
+/***
+ * 关闭窗口
+ * @returns {undefined}
+ */
 Mx.browser.closeWindow = function() {
     if (navigator.userAgent.indexOf("MSIE") > 0) {
         if (navigator.userAgent.indexOf("MSIE 6.0") > 0) {
             window.opener = null;
             window.close();
-        }
-        else {
+        } else {
             window.open('', '_top');
             window.top.close();
         }
-    }
-    else if (navigator.userAgent.indexOf("Firefox") > 0) {
+    } else if (navigator.userAgent.indexOf("Firefox") > 0) {
         window.location.href = 'about:blank ';
         //window.history.go(-2);  
-    }
-    else {
+    } else {
         window.opener = null;
         window.open('', '_self', '');
         window.close();
@@ -619,6 +900,7 @@ $(function() {
 });
 
 /**
+ * 类似C#的字符串格式化函数
  * var a = "我喜欢吃{0}，也喜欢吃{1}，但是最喜欢的还是{0},偶尔再买点{2}";
  * alert(String.format(a, "苹果","香蕉","香梨"));
  * @returns {String.format.str}
